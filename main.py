360b8ae01ce1c67bbefd215f95aeec7cc6c98697
ositaigwe@pursuit.org <j0h@example.com>
2020-04-03 11:59:39 -0400

    read file

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ Opening CSV Files
 from csv import reader 
 
 opened_file = open('/data_sets/HN_posts_year_to_Sep_26_2016.csv')
-read_file = reader(opened_file_apple) 
-apps_data = list(read_file_apple)
+read_file = reader(opened_file) 
+apps_data = list(read_file)
